dfce2396f41d083963dda6dd875923e0cb0887fc
Ylarod <me@ylarod.cn>
2025-11-17 16:46:55 +0800

    support metamodule, remove built-in overlayfs mount (#2929)
    Co-authored-by: weishu <twsxtd@gmail.com>
    Co-authored-by: YuKongA <70465933+YuKongA@users.noreply.github.com>

--- userspace/ksud/src/cli.rs
+++ userspace/ksud/src/cli.rs
@@ -165,19 +165,6 @@ enum Debug {
     /// Get kernel version
     Version,
 
-    Mount,
-
-    /// Copy sparse file
-    Xcp {
-        /// source file
-        src: String,
-        /// destination file
-        dst: String,
-        /// punch hole
-        #[arg(short, long, default_value = "false")]
-        punch_hole: bool,
-    },
-
     /// For testing
     Test,
 
@@ -279,9 +266,6 @@ enum Module {
 
     /// list all modules
     List,
-
-    /// Shrink module image size
-    Shrink,
 }
 
 #[derive(clap::Subcommand, Debug)]
@@ -395,7 +379,6 @@ pub fn run() -> Result<()> {
                 Module::Action { id } => module::run_action(&id),
                 Module::Mount => module::mount_system(),
                 Module::List => module::list_modules(),
-                Module::Shrink => module::shrink_ksu_images(),
             }
         }
         Commands::Install { magiskboot } => utils::install(magiskboot),
@@ -438,15 +421,6 @@ pub fn run() -> Result<()> {
                 Ok(())
             }
             Debug::Su { global_mnt } => crate::su::grant_root(global_mnt),
-            Debug::Mount => init_event::mount_modules_systemlessly(defs::MODULE_DIR),
-            Debug::Xcp {
-                src,
-                dst,
-                punch_hole,
-            } => {
-                utils::copy_sparse_file(src, dst, punch_hole)?;
-                Ok(())
-            }
             Debug::Test => assets::ensure_binaries(false),
             Debug::Mark { command } => match command {
                 MarkCommand::Get { pid } => debug::mark_get(pid),
