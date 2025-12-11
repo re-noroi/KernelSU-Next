62f50b8a4611255569fd1313ec0f1a8a635e8939
rifat azad <rifat.44.azad.rifs@gmail.com>
2025-12-11 23:33:36 +0600

    script: updated userspace build script

--- build.sh
+++ build.sh
@@ -4,23 +4,11 @@
 
 # Ensure you have the setup Android SDK & NDK installed and necessary environment variables set and sourced.
 
-# For LKM make sure you have imported the androidX-X.X_kernelsu.ko drivers to userspace/ksud_*/bin/aarch64 directory.
+# For LKM make sure you have imported the androidX-X.X_kernelsu.ko drivers to userspace/ksud/bin/aarch64 directory.
 
-cross build --target aarch64-linux-android --release --manifest-path ./userspace/ksud_magic/Cargo.toml
+cross build --target aarch64-linux-android --release --manifest-path ./userspace/ksud/Cargo.toml
 
-cp userspace/ksud_magic/target/aarch64-linux-android/release/ksud manager/app/src/main/jniLibs/arm64-v8a/libksud_magic.so
-
-cross build --target aarch64-linux-android --release --manifest-path ./userspace/ksud_overlayfs/Cargo.toml
-
-cp userspace/ksud_overlayfs/target/aarch64-linux-android/release/ksud manager/app/src/main/jniLibs/arm64-v8a/libksud_overlayfs.so
-
-cd userspace/susfsd/jni
-
-ndk-build
-
-cp ../libs/arm64-v8a/susfsd ../../../manager/app/src/main/jniLibs/arm64-v8a/libsusfsd.so
-
-cd ../../..
+cp userspace/ksud/target/aarch64-linux-android/release/ksud manager/app/src/main/jniLibs/arm64-v8a/libksud.so
 
 cd manager
 
