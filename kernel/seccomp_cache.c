a824b9d53fc46653b924da5521cafee54dc4fdb2
rifat azad <rifat.44.azad.rifs@gmail.com>
2025-12-13 03:29:32 +0600

    kernel: remove unused functions and KSU_ALLOWLIST_WORKAROUND guard

--- kernel/seccomp_cache.c
+++ kernel/seccomp_cache.c
@@ -1,40 +1,12 @@
 #include <linux/version.h>
 #include <linux/fs.h>
 #include <linux/nsproxy.h>
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
 #include <linux/sched/task.h>
-#else
-#include <linux/sched.h>
-#endif
 #include <linux/uaccess.h>
 #include <linux/filter.h>
 #include <linux/seccomp.h>
 #include "klog.h" // IWYU pragma: keep
 #include "seccomp_cache.h"
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
-#include <linux/key.h>
-#include <linux/errno.h>
-#include <linux/cred.h>
-struct key *init_session_keyring = NULL;
-
-static inline int install_session_keyring(struct key *keyring)
-{
-	struct cred *new;
-	int ret;
-
-	new = prepare_creds();
-	if (!new)
-		return -ENOMEM;
-
-	ret = install_session_keyring_to_cred(new, keyring);
-	if (ret < 0) {
-		abort_creds(new);
-		return ret;
-	}
-
-	return commit_creds(new);
-}
-#endif
 
 struct action_cache {
 	DECLARE_BITMAP(allow_native, SECCOMP_ARCH_NATIVE_NR);
@@ -44,18 +16,18 @@ struct action_cache {
 };
 
 struct seccomp_filter {
-	refcount_t refs;
-	refcount_t users;
-	bool log;
+    refcount_t refs;
+    refcount_t users;
+    bool log;
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 1, 0)
-	bool wait_killable_recv;
+    bool wait_killable_recv;
 #endif
-	struct action_cache cache;
-	struct seccomp_filter *prev;
-	struct bpf_prog *prog;
-	struct notification *notif;
-	struct mutex notify_lock;
-	wait_queue_head_t wqh;
+    struct action_cache cache;
+    struct seccomp_filter *prev;
+    struct bpf_prog *prog;
+    struct notification *notif;
+    struct mutex notify_lock;
+    wait_queue_head_t wqh;
 };
 
 void ksu_seccomp_clear_cache(struct seccomp_filter *filter, int nr)
@@ -90,27 +62,4 @@ void ksu_seccomp_allow_cache(struct seccomp_filter *filter, int nr)
         set_bit(nr, filter->cache.allow_compat);
     }
 #endif
-}
-
-static inline int ksu_access_ok(const void *addr, unsigned long size)
-{
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,0,0)
-	return access_ok(addr, size);
-#else
-	return access_ok(VERIFY_READ, addr, size);
-#endif
-}
-
-long ksu_strncpy_from_user_retry(char *dst, const void __user *unsafe_addr,
-				   long count)
-{
-	long ret = strncpy_from_user_nofault(dst, unsafe_addr, count);
-	if (likely(ret >= 0))
-		return ret;
-
-	// we faulted! fallback to slow path
-	if (unlikely(!ksu_access_ok(unsafe_addr, count)))
-		return -EFAULT;
-
-	return strncpy_from_user(dst, unsafe_addr, count);
 }
--- kernel/seccomp_cache.h
+++ kernel/seccomp_cache.h
@@ -3,38 +3,6 @@
 
 #include <linux/fs.h>
 #include <linux/version.h>
-#include <linux/list.h>
-
-/**
- * list_count_nodes - count the number of nodes in a list
- * the head of the list
- * 
- * Returns the number of nodes in the list
- */
-#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 6, 0)
-static inline size_t list_count_nodes(const struct list_head *head)
-{
-	const struct list_head *pos;
-	size_t count = 0;
-
-	if (!head)
-		return 0;
-
-	list_for_each(pos, head) {
-		count++;
-	}
-	
-	return count;
-}
-#endif
-
-extern long ksu_strncpy_from_user_retry(char *dst,
-					  const void __user *unsafe_addr,
-					  long count);
-
-#if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0) || defined(CONFIG_IS_HW_HISI) || defined(CONFIG_KSU_ALLOWLIST_WORKAROUND)
-extern struct key *init_session_keyring;
-#endif
 
 extern void ksu_seccomp_clear_cache(struct seccomp_filter *filter, int nr);
 extern void ksu_seccomp_allow_cache(struct seccomp_filter *filter, int nr);
